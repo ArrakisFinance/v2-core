b800a58e4d79dffc2263596cd19f0ec1cb0f1017
gevarist <gevariste@protonmail.com>
2022-09-16 11:34:52 +0200

    deploy: arrakis V2 helper

--- contracts/ArrakisV2Helper.sol
+++ contracts/ArrakisV2Helper.sol
@@ -111,15 +111,58 @@ contract ArrakisV2Helper is IArrakisV2Helper {
         amount0s = new Amount[](ranges_.length);
         amount1s = new Amount[](ranges_.length);
         for (uint256 i = 0; i < ranges_.length; i++) {
-            (uint256 amount0, uint256 amount1) = _getAmountsFromLiquidity(
+            (
+                uint256 amount0,
+                uint256 amount1,
+                ,
+
+            ) = _getAmountsAndFeesFromLiquidity(
+                    token0_,
+                    token1_,
+                    ranges_[i],
+                    vaultV2_
+                );
+
+            amount0s[i] = Amount({range: ranges_[i], amount: amount0});
+            amount1s[i] = Amount({range: ranges_[i], amount: amount1});
+        }
+    }
+
+    function token0AndToken1PlusFeesByRange(
+        Range[] calldata ranges_,
+        address token0_,
+        address token1_,
+        address vaultV2_
+    )
+        external
+        view
+        returns (
+            Amount[] memory amount0s,
+            Amount[] memory amount1s,
+            Amount[] memory fee0s,
+            Amount[] memory fee1s
+        )
+    {
+        amount0s = new Amount[](ranges_.length);
+        amount1s = new Amount[](ranges_.length);
+        fee0s = new Amount[](ranges_.length);
+        fee1s = new Amount[](ranges_.length);
+        for (uint256 i = 0; i < ranges_.length; i++) {
+            amount0s[i].range = ranges_[i];
+            amount1s[i].range = ranges_[i];
+            fee0s[i].range = ranges_[i];
+            fee1s[i].range = ranges_[i];
+            (
+                amount0s[i].amount,
+                amount1s[i].amount,
+                fee0s[i].amount,
+                fee1s[i].amount
+            ) = _getAmountsAndFeesFromLiquidity(
                 token0_,
                 token1_,
                 ranges_[i],
                 vaultV2_
             );
-
-            amount0s[i] = Amount({range: ranges_[i], amount: amount0});
-            amount1s[i] = Amount({range: ranges_[i], amount: amount1});
         }
     }
 
@@ -148,12 +191,21 @@ contract ArrakisV2Helper is IArrakisV2Helper {
 
     // #region internal functions
 
-    function _getAmountsFromLiquidity(
+    function _getAmountsAndFeesFromLiquidity(
         address token0_,
         address token1_,
         Range calldata range_,
         address vaultV2_
-    ) internal view returns (uint256 amount0, uint256 amount1) {
+    )
+        internal
+        view
+        returns (
+            uint256 amount0,
+            uint256 amount1,
+            uint256 fee0,
+            uint256 fee1
+        )
+    {
         (token0_, token1_) = token0_ < token1_
             ? (token0_, token1_)
             : (token1_, token0_);
@@ -162,8 +214,6 @@ contract ArrakisV2Helper is IArrakisV2Helper {
             factory.getPool(token0_, token1_, range_.feeTier)
         );
 
-        uint256 fee0;
-        uint256 fee1;
         (amount0, amount1, fee0, fee1) = UnderlyingHelper.underlying(
             RangeData({self: vaultV2_, range: range_, pool: pool})
         );
