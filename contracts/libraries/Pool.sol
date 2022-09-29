67d95519100065792167065f9c53ba930796647e
gevarist <gevariste@protonmail.com>
2022-09-29 10:07:48 +0200

    chore formatting

--- contracts/libraries/Pool.sol
+++ contracts/libraries/Pool.sol
@@ -10,10 +10,11 @@ import {
 import {Range} from "../structs/SArrakisV2.sol";
 
 library Pool {
-    function validateTickSpacing(
-        address pool_,
-        Range memory range_
-    ) public view returns (bool) {
+    function validateTickSpacing(address pool_, Range memory range_)
+        public
+        view
+        returns (bool)
+    {
         int24 spacing = IUniswapV3Pool(pool_).tickSpacing();
         return
             range_.lowerTick < range_.upperTick &&
