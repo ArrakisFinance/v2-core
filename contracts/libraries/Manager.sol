8aab24ee4df8556d2014bfa39899f6d9ebbf8bb6
gevarist <gevariste@protonmail.com>
2022-09-30 09:35:47 +0200

    manager lib formatting

--- contracts/libraries/Manager.sol
+++ contracts/libraries/Manager.sol
@@ -1,13 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-<<<<<<< HEAD
-import {IManager} from "../interfaces/IManager.sol";
-import {Range} from "../structs/SArrakisV2.sol";
-
-library Manager {
-    function getManagerFeeBPS(IManager manager_) public view returns (uint16) {
-=======
 import {
     IManager
 } from "../interfaces/IManager.sol";
@@ -19,15 +12,10 @@ library Manager {
         view
         returns (uint16)
     {
->>>>>>> 873a3bb (try catch managerFeeBPS)
         try manager_.managerFeeBPS() returns (uint16 feeBPS) {
             return feeBPS;
         } catch {
             return 0;
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 873a3bb (try catch managerFeeBPS)
