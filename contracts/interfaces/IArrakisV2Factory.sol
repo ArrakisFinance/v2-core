c1c7063ce3813009b51d9dccc9b1574a6066d4e1
kassandra <0xdada@protonmail.com>
2022-12-06 09:44:43 -0500

    chore: interface versions

--- contracts/interfaces/IArrakisV2Factory.sol
+++ contracts/interfaces/IArrakisV2Factory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity >=0.8.0;
 
 import {IArrakisV2Beacon} from "./IArrakisV2Beacon.sol";
 import {InitializePayload} from "../structs/SArrakisV2.sol";
