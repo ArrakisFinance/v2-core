b4dba581547b56701375d1810960e2e3a508b0ec
gevarist <gevariste@protonmail.com>
2022-12-23 12:47:03 +0100

    deploy: staging goerli

--- src/addresses.ts
+++ src/addresses.ts
@@ -58,7 +58,7 @@ export const getAddresses = (network: string): Addresses => {
     case "goerli":
       return {
         UniswapV3Factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
-        SwapRouter: "",
+        SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
         TestPool: "",
         WETH: "",
         WMATIC: "",
