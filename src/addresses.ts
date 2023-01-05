8643eaafc5a93e632957ef519eef4998167f39f3
κασσάνδρα.eth <0xDADA@protonmail.com>
2023-01-05 15:02:09 -0500

    chore: add audits (#104)

--- src/addresses.ts
+++ src/addresses.ts
@@ -1,5 +1,3 @@
-import { ethers } from "hardhat";
-
 /* eslint-disable @typescript-eslint/naming-convention */
 export interface Addresses {
   UniswapV3Factory: string;
@@ -34,7 +32,7 @@ export const getAddresses = (network: string): Addresses => {
         UniswapV3Factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
         SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
         WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        WMATIC: ethers.constants.AddressZero.toString(),
+        WMATIC: "",
         USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
         ArrakisV2Implementation: "0xb5C3B286dD591282Fe87Dfab0613488e1b6B09Ba",
         ArrakisV2Beacon: "0x891E7E4baFfeF0ef7bc4b1E85d122bDd7363b8B3",
@@ -60,7 +58,7 @@ export const getAddresses = (network: string): Addresses => {
         UniswapV3Factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
         SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
         WETH: "0x4200000000000000000000000000000000000006",
-        WMATIC: ethers.constants.AddressZero.toString(),
+        WMATIC: "",
         USDC: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607",
         ArrakisV2Implementation: "0xb5C3B286dD591282Fe87Dfab0613488e1b6B09Ba",
         ArrakisV2Beacon: "0x891E7E4baFfeF0ef7bc4b1E85d122bDd7363b8B3",
@@ -73,7 +71,7 @@ export const getAddresses = (network: string): Addresses => {
         UniswapV3Factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
         SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
         WETH: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
-        WMATIC: ethers.constants.AddressZero.toString(),
+        WMATIC: "",
         USDC: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
         ArrakisV2Implementation: "0xb5C3B286dD591282Fe87Dfab0613488e1b6B09Ba",
         ArrakisV2Beacon: "0x891E7E4baFfeF0ef7bc4b1E85d122bDd7363b8B3",
@@ -85,9 +83,9 @@ export const getAddresses = (network: string): Addresses => {
       return {
         UniswapV3Factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
         SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
-        WETH: ethers.constants.AddressZero.toString(),
-        WMATIC: ethers.constants.AddressZero.toString(),
-        USDC: ethers.constants.AddressZero.toString(),
+        WETH: "",
+        WMATIC: "",
+        USDC: "",
         ArrakisV2Implementation: "0xb5C3B286dD591282Fe87Dfab0613488e1b6B09Ba",
         ArrakisV2Beacon: "0x891E7E4baFfeF0ef7bc4b1E85d122bDd7363b8B3",
         ArrakisV2Factory: "0x055B6d3919042Be29C5F044A55529933e1273A88",
