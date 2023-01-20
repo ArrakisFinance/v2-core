816aa8fec7a533011edebb1875bcb2289ac1ff05
irotnes <irotnes@gmail.com>
2023-01-20 15:37:24 -0300

    exporting functions in src/utils in npm package

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 import { sqrt, getNearestTick } from "./sqrt";
+import { getRouters } from "./getRouters";
+import { getPools } from "./getPools";
 
 export const sleep = (ms: number): Promise<void> =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
-export { sqrt, getNearestTick };
+export { sqrt, getNearestTick, getRouters, getPools };
