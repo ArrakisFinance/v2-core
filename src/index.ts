fec0cb19bdfc9b79d7ae720ecc204b398e9af1df
rotnes <rotnes@users.noreply.github.com>
2023-01-20 16:48:35 -0300

    chore: exports and npm config (#123)
    * exporting src/ correctly in npm package
    
    * fixing Addresses type export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export * from "./addresses";
+import { getAddresses, Addresses } from "./addresses";
+import { sqrt, getNearestTick, getRouters, getPools } from "./utils/index";
+
+export { getAddresses, Addresses, sqrt, getNearestTick, getRouters, getPools };
