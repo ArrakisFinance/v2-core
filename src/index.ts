e49dd4fc2caf93b20a7204dbf6498f52aedfa2a9
irotnes <irotnes@gmail.com>
2023-01-20 16:40:44 -0300

    fixing Addresses type export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export * from "./addresses";
-export * from "./utils/index";
+import { getAddresses, Addresses } from "./addresses";
+import { sqrt, getNearestTick, getRouters, getPools } from "./utils/index";
+
+export { getAddresses, Addresses, sqrt, getNearestTick, getRouters, getPools };
