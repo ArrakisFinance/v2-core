cde873d2a90d3bfee854b5c013284340a889089e
kassandra <0xdada@protonmail.com>
2023-01-20 13:59:37 -0500

    chore: use getPools in npm package

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 import { sqrt, getNearestTick } from "./sqrt";
+import { getPools } from "./getPools";
+import { getRouters } from "./getRouters";
 
 export const sleep = (ms: number): Promise<void> =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
-export { sqrt, getNearestTick };
+export { sqrt, getNearestTick, getPools, getRouters };
