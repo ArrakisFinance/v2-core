60988caced4a05f89755b70d817463cd0bff6572
kassandra <0xdada@protonmail.com>
2023-01-27 15:45:41 -0500

    chore: remove unused

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,6 @@
 import { sqrt, getNearestTick } from "./sqrt";
-import { getRouters } from "./getRouters";
-import { getPools } from "./getPools";
 
 export const sleep = (ms: number): Promise<void> =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
-export { sqrt, getNearestTick, getRouters, getPools };
+export { sqrt, getNearestTick };
